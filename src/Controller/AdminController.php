f93fea410cd55bcf3673533c4ca7d04168597a7b
mouncef <zaghratmoncef@gmail.com>
2018-01-15 22:10:59 +0000

    security http

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class AdminController extends Controller
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index()
+    {
+        // replace this line with your own code!
+        return $this->render('@Maker/demoPage.html.twig', [ 'path' => str_replace($this->getParameter('kernel.project_dir').'/', '', __FILE__) ]);
+    }
+}
